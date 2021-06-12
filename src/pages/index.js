15379068127fb0955f61b448f3bd415fc3a9d038
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2021-06-12 16:57:51 +0100

    chore: add stitches to gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
 import React from "react"
 
+import { styled } from 'gatsby-theme-stitches/src/stitches.config';
+
+const Text = styled('p', {
+  fontFamily: '$system',
+  color: '$hiContrast',
+
+  variants: {
+    size: {
+      1: {
+        fontSize: '$1',
+      },
+      2: {
+        fontSize: '$2',
+      },
+      3: {
+        fontSize: '$3',
+      },
+    },
+  },
+});
+
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Text as="h1" size="1">
+      Hello, from Stitches.
+    </Text>
+  );
 }
